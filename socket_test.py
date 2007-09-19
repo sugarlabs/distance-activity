83b0d210fa2d2ceed836ca3452ae7bab88f7fa5f
Benjamin Schwartz <bens@alum.mit.edu>
2007-09-18 23:05:44 -0400

    Add an Interface

---
+++ socket_test.py
@@ -0,0 +1,22 @@
+ss = 'test from server'
+sc = 'test from client'
+
+
+def server(soc):
+    soc.sendall(ss)
+    x = soc.recv(len(sc))
+    return float(hash(x))
+
+def client(soc):
+    x = soc.recv(len(ss))
+    soc.sendall(sc)
+    return float(hash(x))
+
+def speed_of_sound():
+    return 345.67
+
+def measure_dt_seq(soc, is_server):
+    if is_server:
+        return server(soc)
+    else:
+        return client(soc)
