b9c69241dffb4698853ae6e0eee71ddb200cbffe
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2018-05-25 18:40:39 +0530

    Fix PyGI warnings

--- activity.py
+++ activity.py
@@ -20,6 +20,8 @@ import logging
 import telepathy
 import telepathy.client
 import locale
+import gi
+gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
 from gi.repository import Gdk
